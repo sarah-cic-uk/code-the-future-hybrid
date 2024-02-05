4758bff6aeb85a12379141702e87c29e76b3c640
Sarah_N <sarah_n@offstvl.com>
2024-02-05 12:37:47 +0000

    tidying

--- public/pages/js/app.js
+++ public/pages/js/app.js
@@ -1,19 +1,30 @@
 
-function auth(loginBtn, profileBtn){
-    if (localStorage.getItem('loggedIn') === "true") {
-      loginBtn.style.display = "none";
-     profileBtn.style.display = "block";
-    }
-    else {
-      loginBtn.style.display = "block";
-      profileBtn.style.display = "none";
-    }
-    }
-  
-function getPath(){
+function auth(loginBtn, profileBtn, sessionsBtn) {
+  if (localStorage.getItem('loggedIn') === "true") {
+    if (loginBtn) loginBtn.style.display = "none";
+    if (loginBtn) profileBtn.style.display = "block";
+    if (sessionsBtn) sessionsBtn.style.display = "block";
+  }
+  else {
+    if (loginBtn) loginBtn.style.display = "block";
+    if (loginBtn) profileBtn.style.display = "none";
+    if (sessionsBtn) sessionsBtn.style.display = "none";
+  }
+}
+
+function getPath() {
   const BASE_URL = window.location.origin;
   const PATH = BASE_URL.includes('github')
     ? '/code-the-future-hybrid/'
     : BASE_URL.includes('app') ? '/' : '/public/';
-    return PATH;
+  return PATH;
 }
+
+function logout(auth) {
+  auth.signOut().then(() => {
+    console.log("Logged out ")
+    localStorage.setItem('loggedIn', false);
+  }).catch((error) => {
+    console.log("error signing out: ", error.message)
+  });
+};
