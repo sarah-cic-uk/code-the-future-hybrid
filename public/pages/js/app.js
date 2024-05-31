6a986812af1e562731593f0f1655313fcfc0aac8
Sarah_N <sarah_n@offstvl.com>
2024-05-31 09:14:13 +0100

    profile pics working

--- public/pages/js/app.js
+++ public/pages/js/app.js
@@ -99,19 +99,9 @@ async function checkReleaseDates(fbDB) {
   });
 }
 
-function fetchVideo(pathReference, el) {
+function fetchMedia(pathReference, el) {
   pathReference.getDownloadURL()
     .then((url) => {
-      // // This can be downloaded directly:
-      // var xhr = new XMLHttpRequest();
-      // xhr.responseType = 'blob';
-      // xhr.onload = (event) => {
-      //     var blob = xhr.response;
-      // };
-      // xhr.open('GET', url);
-      // xhr.send();
-
-      // Or inserted into an <img> element
       el.setAttribute('src', url);
     })
     .catch((error) => {
