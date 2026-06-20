8e17901a853b323422a87fc0d0bb5c28309eb588
Sarah <sarah.neenan-cic-uk@uk.ibm.com>
2026-06-20 23:51:16 +0100

    migration updates

--- amplify-backend/amplify/auth/resource.ts
+++ amplify-backend/amplify/auth/resource.ts
@@ -3,17 +3,14 @@ import { defineAuth } from '@aws-amplify/backend';
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ *
+ * Note: Keeping configuration minimal because Cognito User Pool
+ * attributes cannot be modified after creation. User display names
+ * will be stored in the DynamoDB User table instead.
  */
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  userAttributes: {
-    // Using Cognito standard attributes
-    // preferred_username will store the display name
-    preferredUsername: {
-      required: false,
-      mutable: true,
-    },
-  },
+  // No userAttributes - store additional user data in DynamoDB instead
 });
